82532bfd14a7a69cf40868a042940c7df81d7fb5
devdras <a.nemeshanyi@gmail.com>
2025-03-14 19:02:42 +0000

    more

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      injectRegister: false,
+      injectRegister: "auto",
 
       pwaAssets: {
         disabled: false,
