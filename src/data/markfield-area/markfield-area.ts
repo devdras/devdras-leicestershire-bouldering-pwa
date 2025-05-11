90f903971e0aba79d4f7be4617169e139e3d05ec
devdras <a.nemeshanyi@gmail.com>
2025-05-11 11:01:52 +0100

    started adding markfeld quarry

--- src/data/markfield-area/markfield-area.ts
+++ src/data/markfield-area/markfield-area.ts
@@ -1,6 +1,9 @@
+import { altarStonesAreaData } from "./sectors/altar-stones";
+import { markfieldQuarryAreaData } from "./sectors/markfield-quarry";
+
 export const markfieldAreaData = {
   name: `markfield-area`,
   displayName: `Markfield Area`,
   overview: `Area in Leicestershire.`,
-  sectors: [],
+  sectors: [altarStonesAreaData, markfieldQuarryAreaData],
 };
---
+++ src/data/markfield-area/sectors/altar-stones.ts
@@ -0,0 +1,81 @@
+export const altarStonesAreaData = {
+  name: `altar-stones-area`,
+  displayName: `Altar Stones Area`,
+  overview: `A little bouldering and a good dyno a short distance outside Markfield and a couple of minutes drive from the M1 (J22). A nice place to stop and break up a journey, or somewhere to pop into after a trip to Forest Rock or Markfield Quarry. The Altar Stones are a biological SSSI so please do not remove lichen from the rocks.`,
+  conditions: ``,
+  approach: `Park at Altar Stones Ln, Markfield LE67 9PX (52.6931, -1.2854). The footpath leads directly to the Altar Stones.`,
+  access: ``,
+  gpsCoordinates: `52.6931, -1.2854`,
+  blocks: [
+    {
+      name: `the-altar`,
+      displayName: `The Altar`,
+      overview: `A quartzite quarry in Bradgate Park with plenty of low to mid-grade bouldering and a small number of harder tight-line problems.  The wall is 6 meters tall at its highest but the landings are flat and grassy.`,
+      conditions: ``,
+      approach: `The closest parking is the Hunts Hill Car Park LE6 0AH.`,
+      access: ``,
+      gpsCoordinates: `52.693861, -1.284661`,
+      sections: [
+        {
+          name: `front-side`,
+          displayName: `Front Side`,
+          routes: [
+            {
+              name: "eucharist",
+              displayName: "Eucharist",
+              grade: "f6c+",
+              description:
+                "Starting on the right-hand block, dyno past the obvious blank projecting wall to the lip (video). Yellow dots on picture.",
+            },
+            {
+              name: "new-kids-on-the-block",
+              displayName: "New Kids on the Block",
+              grade: "f7a",
+              description:
+                "As for Eucharist but starting on the left-hand block. Red dots on picture.",
+            },
+            {
+              name: "pop-culture",
+              displayName: "Pop Culture",
+              grade: "f6a",
+              description:
+                "From the left hand block climb to the top avoiding the big holds round the back of the left arête.​ Blue dots on picture.",
+            },
+          ],
+        },
+        ,
+      ],
+    },
+    {
+      name: `holly-area`,
+      displayName: `Holly Area`,
+      overview: `Hidden away at the north east of the crag close to a dry stone wall.`,
+      conditions: ``,
+      approach: `The closest parking is the Hunts Hill Car Park LE6 0AH.`,
+      access: ``,
+      gpsCoordinates: `52.69392, -1.28425`,
+      sections: [
+        {
+          name: `front-side`,
+          displayName: `Front Side`,
+          routes: [
+            {
+              name: "holly-wall",
+              displayName: "Holly Wall",
+              grade: "f4",
+              description: "Climb the left hand wall.",
+            },
+            {
+              name: "holly-problem-6a",
+              displayName: "Holly Problem 6A",
+              grade: "f6A",
+              description:
+                "Sit start the overhanging featured wall under the holly. Eliminate the arête for bonus points (video).",
+            },
+          ],
+        },
+        ,
+      ],
+    },
+  ],
+};
